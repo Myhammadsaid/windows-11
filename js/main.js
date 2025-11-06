5e98461ba8e694c96ed6d68e36cde1aff1e54837
Myhammadsaid <muhammadsaidmm4@gmail.com>
2025-11-06 22:10:18 +0500

    added sidebar and time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-const menu = document.querySelector('.menu__bar')
-const windows = document.querySelector('.windows')
+const menu__bar = document.querySelector('.menu__bar')
+const wifi__menu = document.querySelector('.wifi__menu')
+const range__menu = document.querySelector('.range__menu')
 
-windows.addEventListener('click', e => {
-	menu.classList.toggle('show')
+document.querySelector('.windows').addEventListener('click', e => {
+	menu__bar.classList.toggle('show')
 })
+
+document.querySelector('#wifi').addEventListener('click', e => {
+	wifi__menu.classList.toggle('show')
+})
+
+document.querySelector('.volume i').addEventListener('click', e => {
+	range__menu.classList.toggle('show')
+})
+
+let range = document.querySelector('#range')
+range.addEventListener('mousemove', e => {
+	document.querySelector('#num').innerHTML = e.target.value
+})
+
+let time = new Date()
+let oclock = `${time.getHours()}:${
+	time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
+} ${time.getHours() <= 12 ? 'AM' : 'PM'}`
+document.querySelector('.oclock').innerHTML = oclock
+
+let date = `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`
+document.querySelector('.date').innerHTML = date
